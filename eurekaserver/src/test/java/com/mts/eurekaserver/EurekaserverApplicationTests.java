0944c577735b7472a66a1d955bfb0e18ce4eee1c
“mtstechnologies” <michael.trindade@hotmail.com>
2024-10-23 18:16:12 -0400

    Discovery Eureka Server created

---
+++ eurekaserver/src/test/java/com/mts/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mts.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
