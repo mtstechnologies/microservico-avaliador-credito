b367ffc9dd69497c414e1b094f93ee64da8c041b
“mtstechnologies” <michael.trindade@hotmail.com>
2024-10-30 18:21:46 -0400

    Credit assessor created and added to the gateway

---
+++ msavaliadorcredito/src/main/java/com/mts/msavaliadorcredito/application/AvaliadorCreditoController.java
@@ -0,0 +1,16 @@
+package com.mts.msavaliadorcredito.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("avaliacoes-credito")
+public class AvaliadorCreditoController {
+	
+	@GetMapping
+	public String status() {
+		return "ok";
+	}
+
+}
